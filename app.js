a6b0e36986f099766e29aa44145380f71dccacca
Denis Stryzhalov <imperor.oktov@gmail.com>
2023-12-11 17:11:23 +0200

    fix

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.use(session(({
  async function start(){
     try{
         mongoose.connect(mongoUrl);
+        
         mongoose.connection.on('connected', () => {
           console.info('Mongo connected', {tags: ['mongo']});
         });
